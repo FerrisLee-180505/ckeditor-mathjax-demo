ce4022fd35228cb1cfc224c023b85238156fba76
Ferris <bin@edmodo.com>
2020-04-15 15:28:18 +0800

    refactor: load cdn lib in js on using

--- src/export/utils/MathjaxUtils.js
+++ src/export/utils/MathjaxUtils.js
@@ -1,3 +1,8 @@
+import $script from 'scriptjs'
+
+// if we need mathjaxUtils, we should load js first.
+$script('http://cdncs.101.com/v0.1/static/dist_learningobjectives_editor/mubiao-static/lib/MathJax/MathJax.js?config=TeX-AMS-MML_SVG-full,local/local')
+
 export default function (element) {
   if (window.MathJax) {
     MathJax.Hub.Queue(['Typeset', MathJax.Hub, element])
