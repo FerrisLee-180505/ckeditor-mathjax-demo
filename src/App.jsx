d130ee1d595c6cd76f169f3647e1a9f78c4cd316
Ferris <bin@edmodo.com>
2020-04-20 13:25:02 +0800

    feature:  demo codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,93 @@
 import React from 'react'
 import RichInput from './export'
+import MathjaxViewer from './export/component/MathjaxViewer'
+import mentions from './../mock/data/mentions'
+import topics from './../mock/data/topics'
 
-class App extends React.Component {
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: ''
+    }
+    this.handleTextChange = this.handleTextChange.bind(this)
+  }
+
+  /**
+   * function callback on value changeed
+   * @param {string} nextText new value of CKEditor
+   */
+  handleTextChange(nextText) {
+    this.setState({
+      text: nextText
+    })
+  }
+
+  getFeedItems(queryText, datas) {
+    // As an example of an asynchronous action, return a promise
+    // that resolves after a 100ms timeout.
+    // This can be a server request or any sort of delayed action.
+    return new Promise(resolve => {
+      setTimeout(() => {
+        const itemsToDisplay = datas
+          // Filter out the full list of all items to only those matching the query text.
+          .filter(item => item.name.toLowerCase().includes(queryText.toLowerCase()))
+          // Return 10 items max - needed for generic queries when the list may contain hundreds of elements.
+          .slice(0, 10)
+
+        resolve(itemsToDisplay)
+      }, 100)
+    })
+  }
+  customItemRenderer(item) {
+    const itemElement = document.createElement('span')
+
+    itemElement.classList.add('custom-item')
+    itemElement.id = `mention-list-item-id-${item.id}`
+    itemElement.textContent = `${item.fullname} `
+
+    const usernameElement = document.createElement('span')
+
+    usernameElement.classList.add('custom-item-username')
+    usernameElement.textContent = item.id
+
+    itemElement.appendChild(usernameElement)
+
+    return itemElement
+  }
+
+
   render() {
+    const { text } = this.state
+    const nextMentions = {
+      feeds: [
+        {
+          marker: '@',
+          feed: keyword => this.getFeedItems(keyword, mentions),
+          itemRenderer: this.customItemRenderer
+        },
+        {
+          marker: '#',
+          feed: keyword => this.getFeedItems(keyword, topics)
+        }
+
+      ]
+    }
     return (
-      <RichInput />
+      <React.Fragment>
+        <RichInput
+          text={text}
+          height={300}
+          useKityformula
+          mentions={nextMentions}
+          handleTextChange={this.handleTextChange}
+        />
+        <h4>The value in CKEditor:</h4>
+        <p>{`${text}`}</p>
+        <h4>The result of display:</h4>
+        <MathjaxViewer text={text} />
+      </React.Fragment>
     )
   }
 }
-
-export default App
--- src/export/component/RichInput.jsx
+++ src/export/component/RichInput.jsx
@@ -1,13 +1,74 @@
 import React, { Component } from 'react'
+import { isEmpty } from 'lodash'
 
+
+console.log('ClassicEditor=', ClassicEditor)
 class RichInput extends Component {
   constructor(props) {
     super(props)
-    this.state = { }
+    this.dom = React.createRef()
   }
+
+  componentDidMount() {
+    const { mentions } = this.props
+    const config = {
+      toolbar: ['heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote', '|', 'MathType', 'ChemType'],
+      mention: mentions
+    }
+    ClassicEditor
+      .create(this.dom.current, config)
+      .then(editor => {
+        this.editor = editor
+
+        if ('disabled' in this.props) {
+          editor.isReadOnly = this.props.disabled
+        }
+
+        if (this.props.onInit) {
+          this.props.onInit(editor)
+        }
+
+        const modelDocument = editor.model.document
+        const viewDocument = editor.editing.view.document
+
+        modelDocument.on('change:data', () => {
+          /* istanbul ignore else */
+          if (this.props.handleTextChange) {
+            this.props.handleTextChange(editor.getData())
+          }
+        })
+
+        viewDocument.on('focus', event => {
+          /* istanbul ignore else */
+          if (this.props.onFocus) {
+            this.props.onFocus(event, editor)
+          }
+        })
+
+        viewDocument.on('blur', event => {
+          /* istanbul ignore else */
+          if (this.props.onBlur) {
+            this.props.onBlur(event, editor)
+          }
+        })
+        const { text } = this.props
+        if (!isEmpty(text)) {
+          editor.setData(text)
+        }
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
   render() {
-    return (<span>RichInput</span>)
+    return (<div ref={this.dom} />)
   }
+}
+
+RichInput.defaultProps = {
+  text: '',
+  mentions: []
 }
 
 export default RichInput
