dfac23e9102b94a0f7404b97f26be00e58c8dddd
Ferris <bin@edmodo.com>
2020-04-24 16:59:00 +0800

    feature: function htmlToMarkdown and markdownToHtml change

--- src/export/utils/MarkdownUtils.js
+++ src/export/utils/MarkdownUtils.js
@@ -1,26 +1,31 @@
 import { map, isEmpty, trim, reduce } from 'lodash'
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { v4 } from 'uuid'
-import showdown from 'showdown'
+import showdown from './../plugin/showdown'
 
-const converter = new showdown.Converter()
+const converter = new showdown.Converter({
+  noHeaderId: true,
+  backslashEscapesHTMLTags: true,
+  emoji: true,
+  splitAdjacentBlockquotes: true
+})
 
 export function htmlToMarkdown(htmlText) {
   // 1.we should tranfered Latex into [math]xxx[/math]
   const { template, latexContent } = convertMathjaxFromHtmlIntoMarkdown(htmlText)
   // 2.we should tranfered html into markdown for save
-  const nextMarkdownText = converter.makeMarkdown(template)
+  const nextMarkdownText = converter.makeMd(template)
   return reduce(latexContent, (rs, value, key) => {
-    return rs.replace(key, `[math]${value}[/math]`)
+    return rs.replace(key, `[math]${value.slice(2, value.length - 2)}[/math]`)
   }, nextMarkdownText)
 }
 export function markdownToHtml(markdownText) {
-  // 1.we should convert Latex into <span class="">xxx</span>
+  // 1.we should convert Latex into <span class="math-tex">xxx</span>
   const { template, latexContent } = convertMathjaxFromMarkdownIntoHtml(markdownText)
   // 2.we should tranfered  markdown into html for re-edit
   const nextHtmlText = converter.makeHtml(template)
   return reduce(latexContent, (rs, value, key) => {
-    return rs.replace(key, `<span class="math-tex">${value}</span>`)
+    return rs.replace(key, `<span class="math-tex">\\(${value}\\)</span>`)
   }, nextHtmlText)
 }
 
