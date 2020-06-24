3ae2dfb58b0e71cdddafc38a9b1699ab9b125f5e
Ferris <bin@edmodo.com>
2020-06-24 17:27:46 +0800

    refactor: Small refactor

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,34 @@ import EDSRichTextInput from './export'
 import { mentions, hashtag } from './datas'
 
 
-const initData = 'this is a **apple** and[edmodo](http://new.edmodo.com),[math]a^2=1[/math]and[math]a^3=1[/math]and[math]a^4=1[/math]todo #face'
+const initData = `# Fibonacci Sequence
+In mathematics, the **Fibonacci numbers**, commonly denoted Fn, form a sequence, called the **Fibonacci sequence**, such that each number is the sum of the two preceding ones, starting from 0 and 1
+This post is about #computerenginerring and #algorithms. Please remember that the #1 thing to remember is to always test your code
+
+this \\*\\*not markdown\\*\\* and \\*also not markdown\\*
+
+This is a ruby implementation:
+\`\`\`
+def fib(n)
+  first_num, second_num = [0, 1]
+  (n - 1).times do
+    first_num, second_num = second_num, first_num + second_num
+  end
+  # just pring the numbers
+  puts first_num
+end
+\`\`\`
+If you want to check all available implementations, please go to [my github](https://nasa.gov)
+
+Things to pay close attention:
+- The fibonnaci sequence starts in 0
+- 1 is the only number in the sequence that repeats itself
+- the reason for that is that 0 + 1 = 1
+    - an indented item
+
+For more info on the fibonacci sequence, please check:
+https://en.wikipedia.org/wiki/Fibonacci_number`
+
 class App extends React.Component {
   constructor(props) {
     super(props)
