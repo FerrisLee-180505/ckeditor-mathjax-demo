9224981973a165c66a6f9c7e3b16fe43c6bc52b4
Ferris <bin@edmodo.com>
2020-07-06 21:48:49 +0800

    styles: styls update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+// === Components === //
+import { Row, Col } from 'reactstrap'
+
 import React from 'react'
 import Markdown from 'react-markdown'
 import EDSRichTextInput from './export'
 
 // === Datas === //
 import { mentions, hashtag } from './datas'
+
+// === Icons === //
+import BoldIcon from './export/icons/BoldIcon'
+import CodeBlockIcon from './export/icons/CodeBlockIcon'
+import HeaderIcon from './export/icons/HeaderIcon'
+import ItalicIcon from './export/icons/ItalicIcon'
+import OrderedListIcon from './export/icons/OrderedListIcon'
+import UnderLineIcon from './export/icons/UnderLineIcon'
+import UnorderedListIcon from './export/icons/UnorderedListIcon'
+import StrikeThroughIcon from './export/icons/StrikeThroughIcon'
+
+
+
+// === Styles === //
+import './styles.css'
 
 
 const initData = `# Fibonacci Sequence
@@ -33,6 +51,17 @@ Things to pay close attention:
 For more info on the fibonacci sequence, please check:
 https://en.wikipedia.org/wiki/Fibonacci_number`
 
+const toolbarItems = [
+  { label: <HeaderIcon />, style: 'header-one' },
+  { label: <UnorderedListIcon />, style: 'unordered-list-item' },
+  { label: <OrderedListIcon />, style: 'ordered-list-item' },
+  { label: <CodeBlockIcon />, style: 'code-block' },
+  { label: <BoldIcon />, style: 'BOLD' },
+  { label: <ItalicIcon />, style: 'ITALIC' },
+  { label: <UnderLineIcon />, style: 'UNDERLINE' },
+  { label: <StrikeThroughIcon />, style: 'STRIKETHROUGH' }
+]
+
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -50,24 +79,31 @@ class App extends React.Component {
   render() {
     const { value } = this.state
     return (
-      <div style={{ backgroundColor: '#eee', padding: 10 }}>
-        <EDSRichTextInput
-          mentions={mentions}
-          hashtag={hashtag}
-          value={value}
-          onChange={this.onChange}
-        />
-        <div>
-          <strong>MARKDOWN - HTML(Best to use EDSRichTextRenderer):</strong>
-          <div style={{ backgroundColor: 'white', borderRadius: '4px', marginTop: '1rem', whiteSpace: 'pre', padding: '4px' }}>
-            <Markdown source={value} className="eds-rich-text-renderer" />
-          </div>
-
-          <strong>DRAFT - MARKDOWN:</strong>
-          <div style={{ backgroundColor: 'white', borderRadius: '4px', marginTop: '1rem', whiteSpace: 'pre-line', padding: '4px' }}>
+      <div>
+        <Row style={{ margin: 0 }}>
+          <Col md={6}>
+            <EDSRichTextInput
+              toolbarItems={toolbarItems}
+              mentions={mentions}
+              hashtag={hashtag}
+              value={value}
+              showBorder
+              onChange={this.onChange}
+              enableMentions
+              enableHashTag
+              enableMathjax
+              toolbarPosition="bottom"
+            />
+          </Col>
+          <Col md={6}>
+            <Markdown source={value} style={{ padding: 14 }} className="eds-rich-text-renderer" />
+          </Col>
+        </Row>
+        <Row style={{ margin: 0 }}>
+          <Col md={12} style={{ backgroundColor: 'white', borderRadius: '4px', marginTop: '1rem', whiteSpace: 'pre-line', padding: '4px' }}>
             {value}
-          </div>
-        </div>
+          </Col>
+        </Row>
       </div>
     )
   }
