74f4053d5087dc2c17ad6ae68bedd73e8d7f15b9
Ferris <bin@edmodo.com>
2020-07-10 11:35:41 +0800

    refactor: Markdown语法解析器解析公式内容逻辑调优

--- src/export/plugins/markdown-edmodo-mathjax/index.js
+++ src/export/plugins/markdown-edmodo-mathjax/index.js
@@ -1,39 +1,46 @@
 /**
  * Plugin for Remarkable Markdown processor which transforms [math]xxxx[/math] sequences into draftjs data
  */
+import trim from 'lodash/trim'
+
 const markdownEdmodoMathjax = (md, options) => {
   const starter = '[math]'
   const ender = '[/math]'
 
   const parseInlineKatex = (state, silent) => {
-    const start = state.pos, max = state.posMax
-    let pos = start
-    if (state.src.charAt(pos) !== '[') return false
-    while (pos < max - starter.length) {
-      if (state.src.slice(pos, pos + starter.length) === starter) {
-        const matchStart = pos
-        let matchEnd = pos + starter.length
-        while (matchEnd < max - ender.length) {
-          if (state.src.slice(matchEnd, matchEnd + ender.length) === ender) {
+    const { src, pos, posMax } = state
+    if (src.charAt(pos) !== '[') return false
+    let pointer = pos
+    // 解析出语法中的[math]开头，并记录位置到matchStart字段
+    while (pointer < posMax - starter.length) {
+      if (src.slice(pointer, pointer + starter.length) === starter) {
+        const matchStart = pointer
+        pointer += starter.length
+        // 指针继续向右偏移，查找语法结束标识符[/math]，并记录位置到matchEnd字段
+        while (pointer <= posMax - ender.length) {
+          if (src.slice(pointer, pointer + ender.length) === ender) {
+            const matchEnd = pointer + ender.length
+            const content = trim(src.slice(matchStart + starter.length, matchEnd - ender.length))
             if (!silent) {
-              const content = state.src.slice(matchStart, matchEnd + ender.length)
-              state.push({ type: 'katex_open', content: content.slice(6, content.length - 7), level: state.level })
+              // 截取起始位置和结束位置中的mathjax内容，推入state对象的token中，用于语法解析
+              state.push({ type: 'katex_open', content: content, level: state.level })
               state.push({ type: 'text', content: '\t\t', level: state.level + 1 })
               state.push({ type: 'katex_close', level: state.level })
             }
-            state.pos = matchEnd + ender.length
-            pos = matchEnd + ender.length
+            // TODO:这里删除了，好像也没啥问题。
+            // state.pos = matchEnd
+            pointer = matchEnd
             break
           }
-          ++matchEnd
+          ++pointer
         }
-        pos = matchEnd + starter.length
-        break
+        // 如果解析到头部，则内部会进行指针偏移设置，如果未解析到头部，则继续向下偏移一个字符
+      } else {
+        ++pointer
       }
-      ++pos
     }
+    return true
   }
-
   md.inline.ruler.push('katex', parseInlineKatex, options)
 }
 export default markdownEdmodoMathjax
